d3c139104b090e5b492b337878bcbda12cfd563a
Shadab Khan B <shadabkhanbenki@gmail.com>
2026-06-30 13:29:26 +0530

    Solved: Pattern 2 (Patterns)

---
+++ patterns/pattern-2/pattern-2.py
@@ -0,0 +1,14 @@
+// Problem: Pattern 2
+// Category: Patterns
+// Difficulty: Easy
+// Link: https://takeuforward.org/plus/dsa/problems/pattern-2?subject=dsa&tab=submissions
+// Solved: 2026-06-30
+//
+// Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:***************Print the pattern in the function given to you.
+
+class Solution:
+    def pattern2(self, n):
+        for i in range(n):
+            for j in range(1+i):
+                print("*", end="")
+            print()
